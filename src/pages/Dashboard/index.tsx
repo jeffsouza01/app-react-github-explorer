3e477d7da18d035f21ac3141d528b06546577298
Jefferson da Silva <je_jss@hotmail.com>
2020-10-04 22:36:15 -0300

    :hammer: include error messages

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -2,23 +2,45 @@ import React, {FormEvent, useState} from 'react';
 import { FiChevronRight } from 'react-icons/fi';
 
 import logoImg from '../../assets/logo.svg';
-import { Title, Form, Repositories } from './styles';
+import { Title, Form, Repositories, Error } from './styles';
 
 import api from '../../services/api';
 
+interface Repository{
+    full_name: string;
+    description: string;
+    owner: {
+        login: string;
+        avatar_url: string;
+    }
+}
+
 const Dashboard: React.FC = () => {
-    const [newRepo, setNetRepo] = useState('');
-    const [repositories, setRepositories] = useState([]);
+    const [newRepo, setNewRepo] = useState('');
+    const [inputError, setInputError ] = useState('');
+    const [repositories, setRepositories] = useState<Repository[]>([]);
 
     async function handleCreateRepository(
         event: FormEvent<HTMLFormElement>,): Promise<void> {
             event.preventDefault();
 
-            const response = await api.get(`repos/${newRepo}`)
+            if(!newRepo) {
+                setInputError('Digite o autor/nome do repositório')
+                return;
+            }
 
-            const repository = response.data;
+            try {
+                const response = await api.get<Repository>(`repos/${newRepo}`)
 
-            //setRepositories([...repositories, repository]);
+                const repository = response.data;
+
+                setRepositories([...repositories, repository]);
+                setNewRepo('');
+                setInputError('');
+            } catch(error) {
+                setInputError('Repositório inválido ou não encontrado');
+            }
+
         }
 
 
@@ -28,28 +50,38 @@ const Dashboard: React.FC = () => {
         <img src={logoImg} alt="Github Explorer"/>
         <Title> Explore repositórios no Github </Title>
 
-        <Form>
-            <input placeholder='Digite aqui o nome do repositório'/>
+        <Form hasError={!!inputError} onSubmit={handleCreateRepository}>
+            <input
+                value={newRepo}
+                onChange={(e) => setNewRepo(e.target.value)}
+                placeholder='Digite aqui o nome do repositório'
+            />
             <button type="submit">Pesquisar</button>
         </Form>
 
+        {inputError && <Error>{inputError} </Error>}
 
         <Repositories>
-            <a href="Testing">
-                <img src="https://avatars0.githubusercontent.com/u/42705353?s=460&u=041225cea197fbaa534cb12c0198973e8cb55ed2&v=4"
-                alt="Jefferson da Silva"/>
+            {repositories.map(repository => (
+            <a key={repository.full_name} href="Testing">
+                <img
+                    src={repository.owner.avatar_url}
+                    alt={repository.owner.login}
+                />
 
                 <div>
-                    <strong>jeffsouza01/api-authentication</strong>
-                    <p>API para criação e autenticação de usuários.</p>
+                    <strong>{repository.full_name}</strong>
+                    <p>{repository.description}</p>
                 </div>
 
                 <FiChevronRight size={30}/>
             </a>
 
+            ))}
+
         </Repositories>
     </>
     )
-)};
+};
 
 export default Dashboard;
