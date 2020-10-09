6cb00abda145a9e9e519b4eb8dddb3eb3de800fd
Jefferson da Silva <je_jss@hotmail.com>
2020-10-09 04:27:43 -0300

    :hammer: update page content to api

--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useRouteMatch, Link } from 'react-router-dom';
 import { FiChevronLeft, FiChevronRight } from 'react-icons/fi'
+import api from '../../services/api'
 
 import { Header, RepositoryInfo, Issues } from './styles';
 import logoImg from '../../assets/logo.svg';
@@ -9,8 +10,44 @@ interface RepositoryParams {
     repository: string;
 }
 
+interface Repository{
+    full_name: string;
+    description: string;
+    stargazers_count: number;
+    forks_count: number;
+    open_issues_count: number;
+    owner: {
+        login: string;
+        avatar_url: string;
+    }
+}
+
+interface Issue {
+    id: number;
+    title: string;
+    html_url: string;
+    user: {
+        login: string;
+    }
+}
+
 const Repository: React.FC = () => {
-    //const { params } = useRouteMatch<RepositoryParams>();
+    const [ repository, setRepository ] = useState<Repository | null >(null);
+    const [ issues, setIssues ] = useState<Issue[]>([]);
+
+    const { params } = useRouteMatch<RepositoryParams>();
+
+    useEffect(() => {
+        api.get(`repos/${params.repository}`).then(response => {
+            setRepository(response.data);
+        });
+
+        api.get(`repos/${params.repository}/issues`).then(response => {
+            setIssues(response.data);
+        });
+
+
+    }, [params.repository]);
 
     return (
         <>
@@ -22,42 +59,52 @@ const Repository: React.FC = () => {
                 </Link>
             </Header>
 
-            <RepositoryInfo >
-                <header>
-                    <img src="https://avatars0.githubusercontent.com/u/42705353?s=460&u=041225cea197fbaa534cb12c0198973e8cb55ed2&v=4" alt="Jeffsouza01"/>
-                    <div>
-                        <strong>jeffsouza01/api-authentication</strong>
-                        <p>descrição do repositorio</p>
-                    </div>
-                </header>
+            { repository && (
 
-                <ul>
-                    <li>
-                        <strong>100</strong>
-                        <span>Stars</span>
-                    </li>
-                    <li>
-                        <strong>48</strong>
-                        <span>Forks</span>
-                    </li>
-                    <li>
-                        <strong>12</strong>
-                        <span>Issues Abertas</span>
-                    </li>
-                </ul>
+                <RepositoryInfo >
+                    <header>
+                        <img
+                            src={repository.owner.avatar_url}
+                            alt={repository.owner.login}
+                            />
+                        <div>
+                            <strong>{repository.full_name}</strong>
+                            <p>{repository.description}</p>
+                        </div>
+                    </header>
 
-            </RepositoryInfo>
+                    <ul>
+                        <li>
+                            <strong>{repository.stargazers_count}</strong>
+                            <span>Stars</span>
+                        </li>
+                        <li>
+                            <strong>{repository.forks_count}</strong>
+                            <span>Forks</span>
+                        </li>
+                        <li>
+                            <strong>{repository.open_issues_count}</strong>
+                            <span>Issues Abertas</span>
+                        </li>
+                    </ul>
+
+                </RepositoryInfo>
+            )}
 
 
             <Issues>
-                <Link to='hohoho'>
-                    <div>
-                        <strong>Teste </strong>
-                        <p>Teste</p>
-                    </div>
 
-                    <FiChevronRight size={20} />
-                </Link>
+                {issues.map((issue) => (
+                    <a key={issue.id} href={issue.html_url} target='_blank' rel="noopener noreferrer">
+                        <div>
+                            <strong>{issue.title} </strong>
+                            <p>{issue.user.login} </p>
+                        </div>
+
+                        <FiChevronRight size={20} />
+                    </a>
+
+                ))}
             </Issues>
         </>
     )
